14c23a0c7c6aa782b23fb76249f7fd5f0b257aa6
JuanCarlosCatalino <juancarloscatalino534@gmail.com>
2024-10-17 13:03:04 -0500

    NO SALIO

--- views/js/functions_productos.js
+++ views/js/functions_productos.js
@@ -1,1 +1,36 @@
-console.log("Hola Mundo");
+async function registrar_producto() {
+    let codigo = document.getElementById('codigo').value;
+    let nombre = document.querySelector('#nombre').value;
+    let detalle = document.querySelector('#detalle').value;
+    let precio = document.querySelector('#precio').value;
+    let stock = document.querySelector('#stock').value;
+    let id_categoria = document.querySelector('#id_categoria').value;
+    let imagen1 = document.querySelector('#imagen1').value;
+    let id_proveedor = document.querySelector('#id_proveedor').value;
+
+    if(codigo==""||nombre==""||detalle==""||precio==""||stock=="" 
+        ||id_categoria==""||imagen1==""||id_proveedor==""){     //par asignar vaolor
+
+            alert("error, campos vacios");
+            return;
+
+    }
+
+    try {
+        const datos = new FormData(frmRegistrar); // datos del formulario
+        // enviar datos hacia el controlador (api)
+        let respuesta = await fetch(base_url+'controller/Producto.php?tipo=registrar',{
+           mothod: 'POST',
+           mode: 'cors',
+           cache: 'no-cache',
+           body: datos 
+        });
+        console.log(respuesta);
+
+
+    } catch (error) {
+        console.log("oops, ocurrioun error"+e);
+    }
+    
+}
+
--- views/nuevo-producto.php
+++ views/nuevo-producto.php
@@ -1,40 +1,45 @@
-<div class="container form-nproduct">
-<form action="">
-    <div>
-        <label for=""> codigo: </label>
-            <input type="number"  class="form-control" >
+<div>
+    <div class="container form-nproduct">
+        <form action="" id ="frmRegistrar">
+            <div>
+                <label for="codigo">Código:</label>
+                <input type="number" id="codigo" class="form-control">
+            </div>
+            <div>
+                <label for="nombre">Nombre:</label>
+                <input type="text" id="nombre" class="form-control">
+            </div>
+            <div>
+                <label for="detalle">Detalle:</label>
+                <input type="text" id="detalle" class="form-control">
+            </div>
+            <div>
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" class="form-control">
+            </div>
+            <div>
+                <label for="stock">Stock:</label>
+                <input type="number" id="stock" class="form-control">
+            </div>
+            <div>
+                <label for="id_categoria">ID Categoría:</label>
+                <input type="number" id="id_categoria" class="form-control">
+            </div>
+            <div>
+                <label for="imagen1">Imagen 1:</label>
+                <input type="text" id="imagen1" class="form-control">
+            </div>
+            <div>
+                <label for="id_proveedor">ID Proveedor:</label>
+                <input type="number" id="id_proveedor" class="form-control">
+            </div>
+            <br>
+            <button type="button" class="btn btn-success"
+            onclick="registrar_producto(); " >Registrar</button>
+        </form>
     </div>
     <div>
-        <label for=""> nombre: </label>
-            <input type="text" class="form-control" >
+        
     </div>
-    <div>
-        <label for=""> detalle: </label>
-            <input type="text" class="form-control" >
-    </div>
-    <div>
-        <label for=""> precio:</label>
-            <input type="number" class="form-control" >
-    </div>
-    <div>
-        <label for=""> stock:</label>
-            <input type="number" class="form-control" >
-    </div>
-    <div>
-        <label for=""> id_categoria: </label>
-            <input type="number"  class="form-control">
-    </div>
-    <div>
-        <label for=""> imagen1: </label>
-            <input type="text" class="form-control" >
-    </div>
-    <div>
-        <label for=""> id_proveedor:</label>
-            <input type="number" class="form-control" >
-    </div>
-    <br>
-    <button type="button" class="btn btn-success">registrar</button>
-    </div>
-</form>
-
-<script src="<?php echo BASE_URL;?>views/js/functions_productos.js"></script>
+    <script src="<?php echo BASE_URL; ?>views/js/functions_productos.js"></script>
+</div>
