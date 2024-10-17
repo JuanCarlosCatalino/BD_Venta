71c0a6d27e50f239369555e5f857c3c7f7f77fda
JuanCarlosCatalino <juancarloscatalino534@gmail.com>
2024-10-17 10:10:13 -0500

    ala

---
+++ views/nuevo-producto.php
@@ -0,0 +1,37 @@
+<div class="container form-nproduct">
+<form action="">
+    <div>
+        <label for=""> codigo: </label>
+            <input type="number"  class="form-control" >
+    </div>
+    <div>
+        <label for=""> nombre: </label>
+            <input type="text" class="form-control" >
+    </div>
+    <div>
+        <label for=""> detalle: </label>
+            <input type="text" class="form-control" >
+    </div>
+    <div>
+        <label for=""> precio:</label>
+            <input type="number" class="form-control" >
+    </div>
+    <div>
+        <label for=""> stock:</label>
+            <input type="number" class="form-control" >
+    </div>
+    <div>
+        <label for=""> id_categoria: </label>
+            <input type="number"  class="form-control">
+    </div>
+    <div>
+        <label for=""> imagen1: </label>
+            <input type="text" class="form-control" >
+    </div><div>
+        <label for=""> id_proveedor:</label>
+            <input type="number" class="form-control" >
+    </div>
+    <br>
+    <button type="button" class="btn btn-success">registrar</button>
+    </div>
+</form>
