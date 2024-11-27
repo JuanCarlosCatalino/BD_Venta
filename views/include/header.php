9e7117e17e8d0de8e791161dba518493accd9bf3
JuanCarlosCatalino <juancarloscatalino534@gmail.com>
2024-11-27 09:08:00 -0500

    12

--- views/include/header.php
+++ views/include/header.php
@@ -8,10 +8,10 @@
   <title>Tazas.com</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-  <link rel="stylesheet" href="<?php echo BASE_URL?>views/css/styles.css">
 <script>
   const base_url ='<?php echo BASE_URL;?>';   //en js en misnucula base url
 </script>
+<link rel="stylesheet" href="<?php echo BASE_URL?>views/css/styles.css">
 </head>
 
 
